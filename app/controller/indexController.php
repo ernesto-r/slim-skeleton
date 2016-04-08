891b8271302984002ced650389d9df3594d154a4
Damian Schwyrz <Damian89@users.noreply.github.com>
2016-04-08 09:38:54 +0200

    Update indexController.php

--- app/controller/indexController.php
+++ app/controller/indexController.php
@@ -5,9 +5,12 @@ namespace App\Controller;
 class indexController extends abstractController {
     
     public function index($request, $response, $args){      
-        $model = new \App\Model\indexModel($this->db, $this->view);
-        $data = ['first', 'second', 'third'];        
-        $model->getData($data);
+        $model = new \App\Model\indexModel($this->db);
+        $data = $model->getData();
+        
+        $view = new \App\View\indexView('template',[
+            'data' => $data
+        ]);
     }
     
 }
