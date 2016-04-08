f6029c7a190480abf760ffc3e18aaf177f4add2e
Damian Schwyrz <Damian89@users.noreply.github.com>
2016-04-08 09:45:36 +0200

    Update template.php

--- app/template/template.php
+++ app/template/template.php
@@ -1,5 +1,5 @@
 <html>
-    <?php $this->insert('header',['head_data' => $data['header']]); ?>
+    <?php $this->insert('header',['header_data' => $data['header']]); ?>
     <body>
         <?php $this->insert('body',['body_data' => $data['body']]); ?>    
     </body>
